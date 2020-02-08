01b613cc41d3571ab78384b1b7e0ac78442c7165
Mariana Bocoi <mariana.bocoi@m.co>
2020-02-08 22:36:04 +0100

    first commit

---
+++ src/test/java/com/pink/crypto/CryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pink.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
